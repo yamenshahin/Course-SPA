b5201c3a2b43f932ff5944688ff14fbfd8334a08
Yamen <yamenshahin@gmail.com>
2024-10-06 18:24:32 +0300

    Cleanup

--- api/index.php
+++ api/index.php
@@ -1,11 +1,3 @@
 <?php
 
 require __DIR__ . '/vendor/autoload.php';
-
-$category = new Category\Controllers\Category();
-echo $category->get()['message'];
-
-echo '<br>';
-
-$course = new Course\Controllers\Course();
-echo $course->get()['message'];
