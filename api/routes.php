e99aa85afa4b1adcc639c8ca0870e3f1b2c32a72
Yamen <yamenshahin@gmail.com>
2024-10-08 23:49:08 +0300

    Fix missing trailing slash at the end of any route error

--- api/routes.php
+++ api/routes.php
@@ -11,23 +11,14 @@ header('Content-Type: application/json');
 
 if ($path[1] != 'categories' && $path[1] != 'courses' && $path[1] != 'courses_by_category') {
     http_response_code(405);
+    echo json_encode(['message' => 'This route does not exist']);
     exit;
 }
 
 switch ($path[1]) {
     case 'categories':
         if ($method === 'GET') {
-            if ($path[2] == null) { // get all categories since there is no id
-                try {
-                    $controller = new CategoryController();
-                    $categories = $controller->getAll();
-                    echo json_encode($categories); // Success case
-                } catch (\Throwable $th) {
-                    // Extract the error message from the returned data
-                    $error_message = $th->getMessage();
-                    echo json_encode(['message' => $error_message]); // Handle the error gracefully
-                }
-            } else { // get category by id
+            if (!empty($path[2])) { // get category by id
                 try {
                     $controller = new CategoryController();
                     $category = $controller->getById($path[2]);
@@ -43,6 +34,16 @@ switch ($path[1]) {
                     $error_message = $th->getMessage();
                     echo json_encode(['message' => $error_message]); // Handle the error gracefully
                 }
+            } else { // get all categories since there is no id
+                try {
+                    $controller = new CategoryController();
+                    $categories = $controller->getAll();
+                    echo json_encode($categories); // Success case
+                } catch (\Throwable $th) {
+                    // Extract the error message from the returned data
+                    $error_message = $th->getMessage();
+                    echo json_encode(['message' => $error_message]); // Handle the error gracefully
+                }
             }
         } else { // method not allowed
             http_response_code(405);
@@ -51,17 +52,7 @@ switch ($path[1]) {
         break;
     case 'courses':
         if ($method === 'GET') {
-            if ($path[2] == null) { // get all courses since there is no id
-                try {
-                    $controller = new CourseController();
-                    $courses = $controller->getAll();
-                    echo json_encode($courses);
-                } catch (\Throwable $th) {
-                    // Extract the error message from the returned data
-                    $error_message = $th->getMessage();
-                    echo json_encode(['message' => $error_message]); // Handle the error gracefully
-                }
-            } else { // get course by id
+            if (!empty($path[2])) { // get course by id
                 try {
                     $controller = new CourseController();
                     $course = $controller->getById($path[2]);
@@ -77,6 +68,16 @@ switch ($path[1]) {
                     $error_message = $th->getMessage();
                     echo json_encode(['message' => $error_message]); // Handle the error gracefully
                 }
+            } else { // get all courses since there is no id
+                try {
+                    $controller = new CourseController();
+                    $courses = $controller->getAll();
+                    echo json_encode($courses);
+                } catch (\Throwable $th) {
+                    // Extract the error message from the returned data
+                    $error_message = $th->getMessage();
+                    echo json_encode(['message' => $error_message]); // Handle the error gracefully
+                }
             }
         } else { // method not allowed
             http_response_code(405);
@@ -85,7 +86,7 @@ switch ($path[1]) {
         break;
     case 'courses_by_category':
         if ($method === 'GET') {
-            if ($path[2] == null) { // get all courses since there is no id
+            if (!empty($path[2])) { // get all courses since there is no id
                 try {
                     $controller = new CourseController();
                     $courses = $controller->getAll();
